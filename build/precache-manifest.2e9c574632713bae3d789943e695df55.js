a578f457f3cab4d9f1d62f1bbe9e80f6c61092d1
syeda-manal <>
2020-06-10 08:30:38 +0500

    add build folder

---
+++ build/precache-manifest.2e9c574632713bae3d789943e695df55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72c7c58f15ebd43c03bfbff5cf23fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caaf15d09cb8efdc0a2d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e1182255fa50111f211c",
+    "url": "/static/js/2.f91eccc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f91eccc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caaf15d09cb8efdc0a2d",
+    "url": "/static/js/main.3fc19975.chunk.js"
+  },
+  {
+    "revision": "012921f555aa8623f900",
+    "url": "/static/js/runtime-main.d37f0636.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
